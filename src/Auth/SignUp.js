d17588193cafec6667ad4034f6a64f205e04eabf
munir79 <jakirhossainmunir79@gmail.com>
2023-08-08 18:04:22 +0600

    log in and sign Up

--- src/Auth/SignUp.js
+++ src/Auth/SignUp.js
@@ -29,7 +29,7 @@ const SignUp = () => {
            
           </div> 
           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <h1 className="text-4xl font-bold ml-10 mt-5 text-orange-300">Login now!</h1>
+          <h1 className="text-4xl font-bold ml-10 mt-5 text-orange-300">Signup now!</h1>
             <div className="card-body">
               <form  onSubmit={handleSignUp}>
               <div className="form-control">
@@ -48,7 +48,7 @@ const SignUp = () => {
                 </label>
               </div>
               <div className="form-control mt-6">
-                <button type='submit' className="btn btn-primary">Login</button>
+                <button type='submit' className="btn btn-primary">SignUp</button>
               </div>
               </form>
             </div>
--- src/Context/UserContext.js
+++ src/Context/UserContext.js
@@ -1,6 +1,6 @@
 import React, { createContext, useState } from 'react';
 import app from '../Firebas/Firebase.init';
-import {createUserWithEmailAndPassword, getAuth} from 'firebase/auth';
+import {createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword} from 'firebase/auth';
 export const AuthContext=createContext();
 const auth=getAuth(app);
 
@@ -11,11 +11,14 @@ const UserContext = ({children}) => {
    const createUser=(email,password)=>{
     return createUserWithEmailAndPassword(auth,email,password);
    }
+   const login=(email,password)=>{
+    return signInWithEmailAndPassword(auth,email,password);
+   }
 
 
     const AuthInfo={
 
-        user,loading,createUser
+        user,loading,createUser,login
     };
     return (
         <AuthContext.Provider value={AuthInfo} >
