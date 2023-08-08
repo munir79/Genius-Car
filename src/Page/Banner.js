a2b6e805c35c9989b58da37de6f9da93a3fbf3e6
munir79 <jakirhossainmunir79@gmail.com>
2023-08-08 10:56:07 +0600

    dynamic banner data

--- src/Page/Banner.js
+++ src/Page/Banner.js
@@ -6,53 +6,53 @@ import img3 from '../assets/images/banner/3.jpg'
 import img4 from '../assets/images/banner/4.jpg'
 import img5 from '../assets/images/banner/5.jpg'
 import img6 from '../assets/images/banner/6.jpg'
+import BannerItem from './BannerItem';
 
 
 const Banner = () => {
+  const banerData=[
+    {
+      image:img1,
+      prev:6,
+      id:1,
+      next:2
+    },
+    {
+      image:img2,
+      prev:1,
+      id:2,
+      next:3
+    },
+    {
+      image:img3,
+      prev:2,
+      id:3,
+      next:4
+    },
+    {
+      image:img4,
+      prev:3,
+      id:4,
+      next:5
+    },
+    {
+      image:img5,
+      prev:4,
+      id:5,
+      next:6
+    },
+    {
+      image:img6,
+      prev:5,
+      id:6,
+      next:1
+    }
+  ]
     return (
         <div className="carousel w-full">
-        <div id="slide1" className="carousel-item relative w-full">
-         <div className='img-gradient'>
-         <img src={img1} alt='' className="w-full" />
-         </div>
-          <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
-            <a href="#slide4" className="btn btn-circle mr-5">❮</a> 
-            <a href="#slide2" className="btn btn-circle">❯</a>
-          </div>
-          <div className="absolute flex justify-end transform -translate-y-1/2 left-24  top-1/4">
-            <h1 className='text-6xl font-bold text-white'> Afordable <br />
-            price car for <br />
-            Servicing</h1>
-          </div>
-          <div className="absolute flex justify-end transform -translate-y-1/2 left-24  w-2/5 top-1/2">
-            <h1 className='text-xl  text-white'> There are many variations of passage of Available,But the majority Have Suffered Altaration in some form  </h1>
-          </div>
-          <div className="absolute flex justify-start transform -translate-y-1/2 left-24  w-2/5 top-3/4">
-          <button className="btn btn-warning mr-5">Warning</button>
-          <button className="btn btn-outline btn-primary">Primary</button>
-          </div>
-        </div> 
-        <div id="slide2" className="carousel-item relative w-full">
-          <img alt='' src="/images/stock/photo-1609621838510-5ad474b7d25d.jpg" className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide1" className="btn btn-circle">❮</a> 
-            <a href="#slide3" className="btn btn-circle">❯</a>
-          </div>
-        </div> 
-        <div id="slide3" className="carousel-item relative w-full">
-          <img src="/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide2" className="btn btn-circle">❮</a> 
-            <a href="#slide4" className="btn btn-circle">❯</a>
-          </div>
-        </div> 
-        <div id="slide4" className="carousel-item relative w-full">
-          <img src="/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide3" className="btn btn-circle">❮</a> 
-            <a href="#slide1" className="btn btn-circle">❯</a>
-          </div>
-        </div>
+        {
+          banerData.map(slide=><BannerItem key={slide.id} slide={slide}></BannerItem>)
+        }
       </div>
     );
 };
---
+++ src/Page/BannerItem.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const BannerItem = ({slide}) => {
+    // console.log(props);
+    const{image,id,next,prev}=slide;
+    
+    return (
+        <div id={`slide${id}`} className="carousel-item relative w-full">
+        <div className='img-gradient'>
+        <img src={image} alt='' className="w-full" />
+        </div>
+         <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+           <a href={`#slide${prev}`} className="btn btn-circle mr-5">❮</a> 
+           <a href={`#slide${next}`} className="btn btn-circle">❯</a>
+         </div>
+         <div className="absolute flex justify-end transform -translate-y-1/2 left-24  top-1/4">
+           <h1 className='text-6xl font-bold text-white'> Afordable <br />
+           price car for <br />
+           Servicing</h1>
+         </div>
+         <div className="absolute flex justify-end transform -translate-y-1/2 left-24  w-2/5 top-1/2">
+           <h1 className='text-xl  text-white'> There are many variations of passage of Available,But the majority Have Suffered Altaration in some form  </h1>
+         </div>
+         <div className="absolute flex justify-start transform -translate-y-1/2 left-24  w-2/5 top-3/4">
+         <button className="btn btn-warning mr-5">Warning</button>
+         <button className="btn btn-outline btn-primary">Primary</button>
+         </div>
+       </div> 
+       
+    );
+};
+
+export default BannerItem;
